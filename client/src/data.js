931690c55f61d38a240eeb8e8a22d1035937c58f
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-05-27 19:57:57 +0530

    Data

---
+++ client/src/data.js
@@ -0,0 +1,26 @@
+export const posts = [
+  {
+    id: 1,
+    title: "5 Premium Theme for React",
+    img: "https://images.pexels.com/photos/7963572/pexels-photo-7963572.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    desc: "ReactJS is a JavaScript library used in web development to build interactive elements on websites.",
+    longDesc:
+      "ReactJS is a JavaScript library used in web development to build interactive elements on websites. Commonly used for building user interfaces, or shortened, UI. Flatlogic React templates offer custom-made themes (free and premium) based on React framework, everything is fully responsive and well tested on most modern devices and browsers. We use modern Javascript and HTML frameworks that have been built by professional developers. Flatlogic team has delivered more than 100 commercial projects in the last 6 years and continues to do so.",
+  },
+  {
+    id: 2,
+    title: "How to Animate Elements ?",
+    img: "https://images.pexels.com/photos/1742370/pexels-photo-1742370.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    desc: "An animation lets an element gradually change from one style to another.",
+    longDesc:
+      "An animation lets an element gradually change from one style to another. You can change as many CSS properties you want, as many times as you want. To use CSS animation, you must first specify some keyframes for the animation. Keyframes hold what styles the element will have at certain times. Also, their are so many libraries for animation. CSS libraries are Animate.CSS, Anim XYZ, Infinite, etc. You can use any of these libraries to create your own animation. Few JavaScript libraries for animation are AniJS, ScrollReveal JS, Popmotion Pure, Lottie JS, MOJS, etc. ",
+  },
+  {
+    id: 3,
+    title: "Learn JS in 30 days",
+    img: "https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    desc: "In order to learn JavaScript, we recommend that you first learn the fundamentals of HTML and CSS.",
+    longDesc:
+      "JavaScript is a programming language. In order to learn JavaScript, we recommend that you first learn the fundamentals of HTML and CSS. Because, as you'll soon see: HTML, CSS and JavaScript always work together at the same time. Together these three languages are known as the front-end languages and they each play a role in building a website. Ready to take an online course? Here are a few online JavaScript courses that I'd recommend for beginners: Learn JS — Whether you are an experienced programmer or a total beginner, this website is a great place to start learning JavaScript. You'll learn the basics of JavaScript like operators, loops, variables, and callbacks. Learn Javascript with One Month — Get JavaScript certified in just 30 days with this online JavaScript coding course. Each week you will be assigned one hour of video content, with an assignment to make a real-world project.",
+  },
+];
