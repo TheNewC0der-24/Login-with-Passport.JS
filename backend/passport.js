9aab47158bddf9e6a9ee20075dfdfcf85065379e
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-02-09 00:15:47 +0530

    Create environment variable

--- backend/passport.js
+++ backend/passport.js
@@ -3,14 +3,23 @@ const GithubStrategy = require("passport-github2").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
-const GOOGLE_CLIENT_ID = "982958489394-tsqc1u42i60e4d9qcghbhuu1i23cp3b4.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-GITHUB_CLIENT_ID = "c7191464fd49ed8476b2";
-GITHUB_CLIENT_SECRET = "86caadeb5520ae280114c036b9651025d4199875";
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
-FACEBOOK_APP_ID = "1140330923413975";
-FACEBOOK_APP_SECRET = "eedf759be0ce688675a62dd7310df3c2";
+const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
+
+// const GOOGLE_CLIENT_ID = "982958489394-tsqc1u42i60e4d9qcghbhuu1i23cp3b4.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_SECRET = "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";
+
+// GITHUB_CLIENT_ID = "c7191464fd49ed8476b2";
+// GITHUB_CLIENT_SECRET = "86caadeb5520ae280114c036b9651025d4199875";
+
+// FACEBOOK_APP_ID = "1140330923413975";
+// FACEBOOK_APP_SECRET = "eedf759be0ce688675a62dd7310df3c2";
 
 passport.use(
   new GoogleStrategy(
